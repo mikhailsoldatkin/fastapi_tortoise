8c2c10dbc7fb7692baf3c08dfd65572760b484ca
michael_soldatkin <michael.soldatkin@gmail.com>
2023-07-17 16:41:03 +0300

    sh script fix

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
-# wait-for-it.sh: Wait until a given host:port is available or timeout
+# wait-for-it.sh: Ожидание доступности указанного хоста:порта или истечения таймаута
+export $(cat .env | xargs)
 
-host="db"
-port="5432"
-timeout="${10:-15}"
+host="$DB_HOST"
+port="$DB_PORT"
+timeout=5
 
-echo "Waiting for $host:$port to be available..."
+echo "Ожидание доступности $host:$port..."
 
 while ! nc -z "$host" "$port"; do
   timeout=$((timeout - 1))
   if [ "$timeout" -eq 0 ]; then
-    echo "Timeout reached. $host:$port is not available."
+    echo "Истекло время ожидания. $host:$port недоступен."
     exit 1
   fi
   sleep 1
 done
 
-echo "$host:$port is available!"
+echo "$host:$port доступен!"
 exit 0
-
